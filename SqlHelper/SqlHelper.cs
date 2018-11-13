7d5d5059fd32d48b46b278b77861fa1f831f6d4a
Yusuf Yılmaz <yusufyilmazfr@gmail.com>
2018-11-13 13:23:34 +0300

    Add: Class

---
+++ SqlHelper/SqlHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Sql_Browser
+{
+    public class SqlHelper
+    {
+        public SqlConnection Connection { get; private set; }
+        public SqlCommand Command { get; private set; }
+
+        public SqlHelper(string connectString)
+        {
+            this.Connection = new SqlConnection(connectString);
+            this.Command = Connection.CreateCommand();
+        }
+
+        public List<string> GetDataList()
+        {
+            List<string> value = new List<string>();
+
+            this.Connection.Open();
+
+            SqlDataReader reader = this.Command.ExecuteReader();
+            while (reader.Read())
+            {
+                value.Add(reader.GetString(0));
+            }
+            reader.Close();
+            reader.Dispose();
+
+            this.Connection.Close();
+
+            return value;
+
+        }
+
+        public DataTable GetDataTable()
+        {
+
+            DataTable myTable = new DataTable();
+            SqlDataAdapter adapter = new SqlDataAdapter(this.Command);
+            adapter.Fill(myTable);
+
+            return myTable;
+        }
+    }
+}
